72d5bcedaeaedd258b392ecc46bb17ce857a9fd7
Mahmud Kandawala <86025767+Mahmud-Kandawala@users.noreply.github.com>
2022-10-26 18:44:16 -0400

    Add files via upload

---
+++ Pay Calculator.py
@@ -0,0 +1,42 @@
+# Input: idNumber, yourHours, payRate, taxRate
+# Calculate: Gross pay, Taxes owed, Netpay, Overtime pay, and Overtime hours
+while True:
+    idName = input("What is your name?: ")
+    
+    yourHours = float(input("\nHow many hours do you work in a week?: "))
+
+    payRate = float(input("\nWhat is your hourly rate?: ")) 
+
+    taxRate = float(input("\nWhat is your tax rate?: "))
+    
+    if yourHours > 40:
+        ovetimeHours = yourHours - 40
+        normalPay = payRate * yourHours
+        ovetimePay = payRate * 1.5 * ovetimeHours
+        grossPay = ovetimePay + normalPay
+        taxesOwed = grossPay / taxRate
+        netPay = grossPay - taxesOwed 
+        
+        print("\nYour name is: ",idName)
+        print("Your gross pay is: ", grossPay)
+        print("The amount you owe in taxes are: ", taxesOwed)
+        print("Your net pay is: ", netPay)
+        
+
+    else:
+        grossPay = payRate * yourHours
+        taxesOwed = grossPay / taxRate
+        netPay = grossPay - taxesOwed
+
+        print("\nYour name is: ", idName)
+        print("Your gross Pay is: ", grossPay)
+        print("The amount you owe in taxes are: ", taxesOwed)
+        print("Your net pay is: ", netPay)
+
+    again = input("\nGo Again? (Y/N): ")
+    if again == "n" or again == "no" or again == "NO" or again == "No":
+        break
+    
+#endif 
+
+#endprogram
